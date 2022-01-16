3576b868b6430ce1f538591ee6a0cb462d98d3a3
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2022-01-16 11:06:55 +0300

    add UserProfile to admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -12,7 +12,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email address')
 
-        email = elf.normalize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
